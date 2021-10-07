de1957f3de2cd99c1207cf09128029529b253a1b
RekaCodes <derrickhudsongreen@gmail.com>
2021-10-07 15:48:32 -0400

    condensed code

--- eda_app.py
+++ eda_app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.random import sample
 import pandas as pd
+import pandas_profiling
 import streamlit as st
 st.set_page_config(layout="wide")
 from streamlit import uploaded_file_manager
@@ -9,21 +10,13 @@ from streamlit_pandas_profiling import st_profile_report
 
 def main():   
 
-    # data
-    sample_data = pd.DataFrame(
-        np.random.rand(100,8),
-        columns=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    )
-
     # sidebar
-
     st.sidebar.header('File Uploader')
     # st.sidebar.write('---')
     data_file = st.sidebar.file_uploader('Upload a file or select the sample dataset below.')
 
     
     # app
-
     st.write('''
         # EDA Web App
 
@@ -44,20 +37,22 @@ def main():
         st.write('### EDA Report')
         st_profile_report(report)
 
+    elif st.sidebar.button('Click for sample dataset.'):
+        @st.cache
+        def load_data():
+            sample_df = pd.read_csv('titanic_sample_data.csv')
+            return sample_df
+        df = load_data()
+        report = ProfileReport(df, explorative=True) 
+        st.write('### Input Data')
+        st.dataframe(df)
+        st.write('---')
+        st.write('### EDA Report')
+        st_profile_report(report)
+
     else:
-        # st.info('To get started, use the File Uploader in the Sidebar.')
-        if st.sidebar.button('Click for sample dataset.'):
-            @st.cache
-            def load_data():
-                sample_df = pd.read_csv('titanic_sample_data.csv')
-                return sample_df
-            df = load_data()
-            report = ProfileReport(df, explorative=True) 
-            st.write('### Input Data')
-            st.dataframe(df)
-            st.write('---')
-            st.write('### EDA Report')
-            st_profile_report(report)
+        st.info('Upload data to begin.')
+
 
 
 if __name__ == "__main__":
